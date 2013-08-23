a13eda45f8171935bb4b84595315786c8cca70e5
Arturo Espinosa <arturo.espinosa@xamarin.com>
2013-08-22 23:47:48 -0500

    Label with a shadow.
    Port of the label code from the Facebook-iOS library.

---
+++ Source/Facebook.Client-iOS/Facebook.Client-iOS/Controls/LoginButton/ShadowLabel.cs
@@ -0,0 +1,33 @@
+using System;
+using MonoTouch.UIKit;
+using MonoTouch.CoreGraphics;
+using System.Drawing;
+
+namespace Facebook.Client.Controls
+{
+	public class ShadowLabel: UILabel
+	{
+		public ShadowLabel () : base ()
+		{
+		}
+
+		public void DrawTextInRect (RectangleF rect) 
+		{
+			SizeF myShadowOffset = new SizeF (0, -1);
+			float[] myColorValues = {0, 0, 0, 0.3};
+
+			using (CGContext myContext = UIGraphics.GetCurrentContext ())
+			{
+				myContext.SaveState ();
+				using (CGColorSpace myColorSpace = CGColorSpace.CreateDeviceRGB ())
+					using (CGColor myColor = new CGColor (myColorSpace, myColorValues))
+					{	
+						myContext.SetShadowWithColor (myShadowOffset, 1, myColor);
+						base.DrawText (rect);
+					}
+			}
+		}
+
+	} // class ShadowLabel
+}
+
