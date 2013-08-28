289c401baa90e853d422ad8e58109c28b5b3d288
Arturo Espinosa <arturo.espinosa@xamarin.com>
2013-08-28 18:07:07 -0500

    OAuth methods implemented.
    Also changed SetupUI to PresentUI.

--- Source/Facebook.Client-iOS/Facebook.Client-iOS/FacebookAuthenticator.cs
+++ Source/Facebook.Client-iOS/Facebook.Client-iOS/FacebookAuthenticator.cs
@@ -1,11 +1,10 @@
 using System;
-using Xamarin.Auth;
 
 namespace Facebook.Client
 {
 	public partial class FacebookAuthenticator
 	{
-		public void SetupUI ()
+		public void PresentUI ()
 		{
 		}
 
--- Source/Facebook.Client-iOS/Facebook.Client/FacebookAuthenticator.cs
+++ Source/Facebook.Client-iOS/Facebook.Client/FacebookAuthenticator.cs
@@ -1,13 +1,44 @@
 using System;
+using System.Threading.Tasks;
 using Xamarin.Auth;
 
 namespace Facebook.Client
 {
 	public partial class FacebookAuthenticator : OAuth2Authenticator
 	{
-		public FacebookAuthenticator (string appId, Uri startUrl, Uri endUrl): base (appId, "", startUrl, endUrl) 
+		public FacebookAuthenticator (string appId, Uri startUri, Uri endUri) : base (appId, "", startUri, endUri)
 		{
+			this.AllowCancel = true;
+			this.ClearCookiesBeforeLogin = false;
+		}
+
+		public async Task<Account> AuthenticateAsync ()
+		{
+			TaskCompletionSource<Account> tcs = new TaskCompletionSource<Account>();
+
+			this.Error += (object sender, AuthenticatorErrorEventArgs e) => {
+				// Platform-specific UI routine.
+				this.DismissUI ();
+
+				tcs.TrySetException (new InvalidOperationException (e.Message));
+			};
+
+			this.Completed += (object sender, AuthenticatorCompletedEventArgs e) => {
+				// Platform-specific UI routine.
+				this.DismissUI ();
+
+				if (!e.IsAuthenticated) {
+					tcs.TrySetException (new InvalidOperationException ());
+					return;
+				}
+
+				tcs.TrySetResult (e.Account);
+			};
+
+			// Platform-specific UI routine.
+			this.PresentUI ();
+
+			return tcs.Task;
 		}
 	}
 }
-
