cd7fdcb7eab1ac83ed8b695b3458ee807b747c18
Arturo Espinosa <arturo.espinosa@xamarin.com>
2013-08-29 18:01:55 -0500

    Finished Controls/ProfilePicture initial implementation (theoretic).

---
+++ Source/Facebook.Client-iOS/Facebook.Client-iOS/Controls/ProfilePicture/ProfilePicture.cs
@@ -0,0 +1,106 @@
+using System;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using System.Drawing;
+
+namespace Facebook.Client.Controls
+{
+	public partial class ProfilePicture: UIView
+	{
+		private UIImageView ImageView;
+		private static float ScreenScaleFactor = 0.0;
+		private HttpHelper connection = null;
+
+		/// <summary>
+		/// Initializes a new instance of the ProfilePicture class.
+		/// </summary>
+		public ProfilePicture()
+		{
+			this.ImageView = new UIImageView (this.Bounds);
+			this.ImageView.AutoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight;
+
+			this.AutosizesSubviews = true;
+			this.ClipsToBounds = true;
+
+			this.AddSubview (this.ImageView);
+		}
+
+		// Lets us catch resizes of the control, or any outer layout, allowing us to potentially
+		// choose a different image.
+		public override void LayoutSubviews ()
+		{
+			this.EnsureImageViewContentMode ();
+			base.LayoutSubviews ();
+		}
+
+		private float GetWidth () {
+			if (ScreenScaleFactor == 0.0) {
+				ScreenScaleFactor = UIScreen.MainScreen.Scale;
+			}
+
+			// Retina display doesn't increase the bounds that iOS returns.  The size needs
+			// to be calculated using the scale factor accessed above.
+			return this.Bounds.Width * ScreenScaleFactor;
+		}
+
+		private float GetHeight () {
+			if (ScreenScaleFactor == 0.0) {
+				ScreenScaleFactor = UIScreen.MainScreen.Scale;
+			}
+
+			// Retina display doesn't increase the bounds that iOS returns.  The size needs
+			// to be calculated using the scale factor accessed above.
+			return this.Bounds.Height * ScreenScaleFactor;
+		}
+
+		private void CropModeChanged () {
+			this.LoadPicture ();
+		}
+
+		private void RefreshImage () {
+			if (string.IsNullOrEmpty (this.ProfileId)) {
+				if (this.connection) {
+					this.connection.CancelAsync ();
+				}
+
+				this.connection = new HttpHelper (this.ImageSource);
+				this.connection.OpenReadTaskAsync ().ContinueWith (t => {
+					if (t.IsCompleted) {
+						using (var stream = t.Result) {
+							using (var data = NSData.FromStream (stream)) {
+								this.ImageView.Image = UIImage.LoadFromData (data);
+								this.EnsureImageViewContentMode ();
+							}
+						}
+					}
+					this.connection = null;
+				});
+
+			} else {
+				this.ImageView.Image = UIImage.FromBundle (this.ImageSource);
+				this.EnsureImageViewContentMode ();
+			}
+		}
+
+		private void EnsureImageViewContentMode () {
+			// Set the image's contentMode such that if the image is larger than the control, we scale it down, preserving aspect 
+			// ratio.  Otherwise, we center it.  This ensures that we never scale up, and pixellate, the image.
+			SizeF viewSize = this.Bounds.Size;
+			SizeF imageSize = this.ImageView.Image.Size;
+			UIViewContentMode contentMode;
+
+			// If both of the view dimensions are larger than the image, we'll center the image to prevent scaling up.
+			// Note that unlike in choosing the image size, we *don't* use any Retina-display scaling factor to choose centering
+			// vs. filling.  If we were to do so, we'd get profile pics shrinking to fill the the view on non-Retina, but getting
+			// centered and clipped on Retina.  
+			if (viewSize.Width > imageSize.Width && viewSize.Height > imageSize.Height) {
+				contentMode = UIViewContentMode.Center;
+			} else {
+				contentMode = UIViewContentMode.ScaleAspectFit;
+			}
+
+			this.ImageView.ContentMode = contentMode;
+		}
+	}
+}
+
---
+++ Source/Facebook.Client-iOS/Facebook.Client/Controls/ProfilePicture/ProfilePicture.cs
@@ -0,0 +1,179 @@
+﻿namespace Facebook.Client.Controls
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+
+    /// <summary>
+    /// Shows the profile picture for an object such as a user, place, or event.
+    /// </summary>
+    public partial class ProfilePicture
+    {
+        #region Default Property Values
+
+        private const string DefaultAccessToken = "";
+        private const string DefaultProfileId = "";
+        private const CropMode DefaultCropMode = CropMode.Original;
+
+        #endregion Default Property Values
+
+		#region Member variables
+
+		private string _profileId = DefaultProfileId;
+		private CropMode _cropMode = DefaultCropMode;
+		private string _imageSource = "";
+
+		#endregion Member variables
+
+		#region Properties
+
+        #region AccessToken
+        
+        /// <summary>
+        /// Gets or sets the access token returned by the Facebook Login service.
+        /// </summary>
+		public string AccessToken { get; set; }
+
+        #endregion AccessToken
+        
+        #region ProfileId
+
+        /// <summary>
+        /// The Facebook ID of the user, place or object for which a picture should be fetched and displayed.
+        /// </summary>
+        /// <remarks>
+        /// The control displays a blank profile (silhouette) picture if this property is null or empty.
+        /// </remarks>
+		public string ProfileId { 
+			get { return this._profileId; }
+			set {
+				this._profileId = value;
+				this.LoadPicture ();
+			} 
+		}
+
+		#endregion ProfileId
+
+        #region CropMode
+        
+        /// <summary>
+        /// Gets or sets the cropping treatment of the profile picture.
+        /// </summary>
+        public CropMode CropMode
+        {
+			get { return this._cropMode; }
+			set {
+				this._cropMode = value;
+				this.CropModeChanged ();
+			}
+        }
+        
+        #endregion CropMode
+
+		#region ImageSource
+
+		private string ImageSource { 
+			get { return this._imageSource; }
+			set {
+				this._imageSource = value;
+				this.RefreshImage ();
+			}
+		}
+
+		#endregion ImageSource
+
+		#region Width
+
+		private float Width
+		{ 
+			get { return this.GetWidth (); } 
+		}
+
+		#endregion Width
+
+		#region Height
+
+		private float Height 
+		{ 
+			get { return this.GetHeight (); } 
+		}
+
+		#endregion Height
+
+		#endregion Properties
+
+        #region Implementation
+		
+        private void LoadPicture()
+        {
+            string profilePictureUrl;
+
+            if (string.IsNullOrEmpty(this.ProfileId))
+            {
+                profilePictureUrl = ProfilePicture.GetBlankProfilePictureUrl(this.CropMode == CropMode.Square);
+            }
+            else
+            {
+                profilePictureUrl = this.GetFacebookProfilePictureUrl();
+            }
+
+			if (this.ImageSource != profilePictureUrl) 
+			{
+				this.ImageSource = profilePictureUrl;
+			}
+        }
+
+        private string GetFacebookProfilePictureUrl()
+        {
+            string profilePictureUrl;
+            const string GraphApiUrl = "https://graph.facebook.com";
+
+            if (this.CropMode == CropMode.Square)
+            {
+                var size = Math.Max(this.Height, this.Width);
+                profilePictureUrl = string.Format(
+                                        CultureInfo.InvariantCulture,
+                                        "{0}/{1}/picture?width={2}&height={3}",
+                                        GraphApiUrl,
+                                        this.ProfileId,
+                                        size,
+                                        size);
+            }
+            else
+            {
+                profilePictureUrl = string.Format(
+                                        CultureInfo.InvariantCulture,
+                                        "{0}/{1}/picture?width={2}&height={3}",
+                                        GraphApiUrl,
+                                        this.ProfileId,
+                                        this.Width,
+                                        this.Height);
+            }
+
+            if (!string.IsNullOrEmpty(this.AccessToken))
+            {
+                profilePictureUrl += "&access_token=" + this.AccessToken;
+            }
+
+            return profilePictureUrl;
+        }
+
+        internal static string GetBlankProfilePictureUrl(bool isSquare)
+        {
+            const string BlankProfilePictureSquare = "fb_blank_profile_square.png";
+            const string BlankProfilePicturePortrait = "fb_blank_profile_portrait.png";
+
+            string imageName = isSquare ? BlankProfilePictureSquare : BlankProfilePicturePortrait;
+#if NETFX_CORE
+            var libraryName = typeof(ProfilePicture).GetTypeInfo().Assembly.GetName().Name;
+
+            return string.Format(CultureInfo.InvariantCulture, "ms-appx:///{0}/Images/{1}", libraryName, imageName);
+#endif
+#if WINDOWS_PHONE || __MOBILE__
+            return string.Format(CultureInfo.InvariantCulture, "/Images/{0}", imageName);
+#endif
+        }
+
+        #endregion Implementation
+    }
+}
