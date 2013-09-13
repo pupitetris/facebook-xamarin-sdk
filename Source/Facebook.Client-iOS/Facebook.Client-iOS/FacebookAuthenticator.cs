6933c83322a90498aaa1d776b2acddfdfc096fb5
Arturo Espinosa <arturo.espinosa@xamarin.com>
2013-09-13 18:03:32 -0500

    Usage comments for later documentation.

--- Source/Facebook.Client-iOS/Facebook.Client-iOS/FacebookAuthenticator.cs
+++ Source/Facebook.Client-iOS/Facebook.Client-iOS/FacebookAuthenticator.cs
@@ -8,7 +8,7 @@ namespace Facebook.Client
 {
 	public partial class FacebookAuthenticator
 	{
-		public object ParentUI { get; set; }
+		public object ParentUI { get; set; } // an UIView can be set for iPad. iPhone and iPod require an UIViewController.
 		public RectangleF PresentRect { get; set; }
 
 		private UIPopoverController Popover = null;
@@ -20,14 +20,14 @@ namespace Facebook.Client
 
 		public void PresentUI ()
 		{
-			var controller = this.ParentUI as UIViewController;
+			var controller = this.ParentUI as UIViewController; // iPhone and iPod must use this.
 			if (controller != null)
 			{
 				controller.PresentViewController (this.GetUI (), true, null);
 				return;
 			}
 
-			UIView v = this.ParentUI as UIView;
+			UIView v = this.ParentUI as UIView; // only for iPad.
 			UIBarButtonItem barButton = this.ParentUI as UIBarButtonItem;
 
 			this.Popover = new UIPopoverController (this.GetUI ());
