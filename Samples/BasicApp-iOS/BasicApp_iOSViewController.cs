39fef4c19d2c287d999818d29edfb64c46698018
Arturo Espinosa <arturo.espinosa@xamarin.com>
2013-09-13 20:46:29 -0500

    Basic example with the login button for now.

--- Samples/BasicApp-iOS/BasicApp_iOSViewController.cs
+++ Samples/BasicApp-iOS/BasicApp_iOSViewController.cs
@@ -2,11 +2,14 @@ using System;
 using System.Drawing;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
+using Facebook.Client.Controls;
 
 namespace BasicAppiOS
 {
 	public partial class BasicApp_iOSViewController : UIViewController
 	{
+		protected LoginButton MyLoginButton;
+
 		public BasicApp_iOSViewController () : base ("BasicApp_iOSViewController", null)
 		{
 		}
@@ -24,12 +27,43 @@ namespace BasicAppiOS
 			base.ViewDidLoad ();
 			
 			// Perform any additional setup after loading the view, typically from a nib.
+			MyLoginButton = new LoginButton ();
+			LoginButtonCont.AddSubview (MyLoginButton);
+
+			MyLoginButton.ParentUI = this;
+			MyLoginButton.SessionStateChanged += this.OnSessionStateChanged;
+			MyLoginButton.ApplicationId = "427365490674294";
 		}
 
-		public override bool ShouldAutorotateToInterfaceOrientation (UIInterfaceOrientation toInterfaceOrientation)
+		private void OnSessionStateChanged(object sender, Facebook.Client.Controls.SessionStateChangedEventArgs e)
 		{
-			// Return true for supported orientations
-			return (toInterfaceOrientation != UIInterfaceOrientation.PortraitUpsideDown);
+			string msg = "Other?";
+
+			switch (e.SessionState) {
+			case FacebookSessionState.Closed:
+				msg = "Closed.";
+				break;
+			case FacebookSessionState.ClosedLoginFailed:
+				msg = "Logout failed.";
+				break;
+			case FacebookSessionState.Created:
+				msg = "Just initialized.";
+				break;
+			case FacebookSessionState.CreatedTokenLoaded:
+				msg = "Just initialized, we have a stored session";
+				break;
+			case FacebookSessionState.Opened:
+				msg = "Opened. Welcome!";
+				break;
+			case FacebookSessionState.OpenedTokenUpdated:
+				msg = "Opened with stored session. Welcome!";
+				break;
+			case FacebookSessionState.Opening:
+				msg = "Session is being opened.";
+				break;
+			}
+			Console.Write ("SessionStateChanged: ");
+			Console.WriteLine (msg);
 		}
 	}
 }
--- Samples/BasicApp-iOS/BasicApp_iOSViewController.designer.cs
+++ Samples/BasicApp-iOS/BasicApp_iOSViewController.designer.cs
@@ -1,17 +1,26 @@
+// WARNING
 //
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
 using MonoTouch.Foundation;
+using System.CodeDom.Compiler;
 
 namespace BasicAppiOS
 {
 	[Register ("BasicApp_iOSViewController")]
 	partial class BasicApp_iOSViewController
 	{
+		[Outlet]
+		MonoTouch.UIKit.UIView LoginButtonCont { get; set; }
+		
 		void ReleaseDesignerOutlets ()
 		{
+			if (LoginButtonCont != null) {
+				LoginButtonCont.Dispose ();
+				LoginButtonCont = null;
+			}
 		}
 	}
 }
