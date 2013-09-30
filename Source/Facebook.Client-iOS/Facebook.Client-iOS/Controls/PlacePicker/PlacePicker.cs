c0ffd57bcda3bfeef96d11bc802c25e8d4c4b41b
Arturo Espinosa <arturo.espinosa@xamarin.com>
2013-09-29 19:33:18 -0500

    Added cell fetching routine for PlacePicker.
    Will refine later with an image and more detail.

--- Source/Facebook.Client-iOS/Facebook.Client-iOS/Controls/PlacePicker/PlacePicker.cs
+++ Source/Facebook.Client-iOS/Facebook.Client-iOS/Controls/PlacePicker/PlacePicker.cs
@@ -1,10 +1,26 @@
 using System;
 using System.Drawing;
+using MonoTouch.UIKit;
+using MonoTouch.Foundation;
 
 namespace Facebook.Client.Controls
 {
 	public partial class PlacePicker
 	{
+		private static readonly string CellIdentifier = "FacebookClient.PlacePicker";
+
+		public override UITableViewCell GetCell (UITableView tableView, NSIndexPath indexPath)
+		{
+			UITableViewCell cell = tableView.DequeueReusableCell (CellIdentifier);
+
+			// if there are no cells to reuse, create a new one
+			if (cell == null) {
+				cell = new UITableViewCell (UITableViewCellStyle.Default, CellIdentifier);
+				GraphPlace place = Items [indexPath.Row];
+				cell.TextLabel.Text = place.Name;
+			}
+			return cell;
+		}
 	}
 }
 
